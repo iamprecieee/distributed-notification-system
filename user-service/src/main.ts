43a90d7344608a9d20ec543bc25e6c5cead1e065
Elijah Arhinful <elijaharhinful8@gmail.com>
2025-11-13 03:34:40 +0000

    docs(user-service): update to user service flow

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -55,11 +55,6 @@ async function bootstrap() {
       },
       'JWT-auth',
     )
-    .addServer(
-      `http://localhost:${configService.get('port')}`,
-      'Local Development',
-    )
-    .addServer('https://api.yourdomain.com', 'Production')
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
@@ -76,10 +71,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin:
-      configService.get('environment') === 'development'
-        ? '*'
-        : ['https://yourdomain.com'],
+    origin: '*',
     credentials: true,
   });
 
